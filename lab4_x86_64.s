803e47e1b68dd832e71d03c9d45436e2eec45c04
Carmen W <Carmenwhitton@gmail.com>
2023-11-10 20:20:58 -0500

    x86_64 part 4

--- lab4_x86_64.s
+++ lab4_x86_64.s
@@ -2,29 +2,33 @@
 .globl    _start
 
 min = 0                         /* starting value for the loop index; note that this is a symbol (constant), not a variable */
-max = 10                        /* loop exits when the index hits this number (loop condition is i<max) */
+max = 31                        /* loop exits when the index hits this number (loop condition is i<max) */
 
 _start:
     mov     $min,%r15           /* loop index */
 
 loop:
-    mov %r15,%r14               /* store the current loop value for conversion */
-    add $48,%r14                /* convert to ascii character */
+    mov $0,%rdx                 /* rdx needs to be 0 before division */
+    mov %r15,%rax               /* store the current loop value for conversion */
+    mov $10, %r14               /* store 10 (division and newline operator) */
+    div %r14                    /* divide the index by 10, quotient in rax, remainder rdx */
 
-    mov $len,%rdx               /* message length */
-    mov $msg,%rsi               /* message location */
+    add $48,%rax                /* convert high digit into ascii */
+    add $48,%rdx                /* convert low digit into ascii */
 
     mov $msg,%r13               /* store write append location */
     add $len,%r13               /* go to the end of the msg */
     add $1,%r13                 /* add 1 to length to append character */
 
-    mov %r14,(%r13)             /* move loop index character to end of message */
-    mov $10,%r14                /* set newline character in r14 */
+    mov %rax,(%r13)             /* move loop index high digit character to end of message */
     add $1,%r13                 /* go to next message location */
-    mov %r14,(%r13)             /* append newline character to msg */
+    mov %rdx,(%r13)             /* append loop index low digit character to msg */
+    add $1,%r13                 /* got to next message location */
+    mov %r14,(%r13)             /* append newline character */
 
-    add $3,%rdx                 /* Add 2 to length */
-
+    mov $len,%rdx               /* message length */
+    add $4,%rdx                 /* Add to length */
+    mov $msg,%rsi               /* message location */
     mov $1,%rdi                 /* file descriptor stdout */
     mov $1,%rax                 /* sys_write */
     syscall
