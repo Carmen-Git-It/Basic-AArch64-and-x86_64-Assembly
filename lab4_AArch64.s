800e5883378b796989df66b3ebf09bb81ca9e3d3
Carmen W <Carmenwhitton@gmail.com>
2023-11-10 19:27:20 -0500

    AArch64 part 1

--- lab4_AArch64.s
+++ lab4_AArch64.s
@@ -14,8 +14,20 @@ loop:
 
     add     x19, x19, 1
     cmp     x19, max
+
+    mov x0, 1           /* file descriptor: 1 is stdout */
+    adr x1, msg         /* message location (memory address) */
+    mov x2, len         /* message length (bytes) */
+
+    mov x8, 64          /* write is syscall #64 */
+    svc 0               /* invoke syscall */
+
     b.ne    loop
 
     mov     x0, 0           /* status -> 0 */
     mov     x8, 93          /* exit is syscall #93 */
     svc     0               /* invoke syscall */
+
+.data
+msg:    .ascii  "Loop\n"
+len=    . - msg
